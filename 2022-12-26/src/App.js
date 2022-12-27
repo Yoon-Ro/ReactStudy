70f98f0f332c84ecd7bdae807ec854f95870d120
yoonro1995 <87830420+yoonro1995@users.noreply.github.com>
2022-12-26 22:21:35 -0800

    Yea

---
+++ 2022-12-26/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1><a href="#">WEB</a></h1>
+      </header>
+      <nav>
+        <ol>
+          <li><a href="/read/1">html</a></li>
+          <li><a href="/read/2">css</a></li>
+          <li><a href="/read/3">JavaScript</a></li>
+        </ol>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
