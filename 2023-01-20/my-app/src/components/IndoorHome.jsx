bebef4e00402c3fd22ddc2a41886a276ed1a5ff6
Yoon Ro <87830420+yoonro1995@users.noreply.github.com>
2023-01-26 14:46:45 -0800

    GIT Catalog
    import json data into jsx.

--- 2023-01-20/my-app/src/components/IndoorHome.jsx
+++ 2023-01-20/my-app/src/components/IndoorHome.jsx
@@ -1,22 +1,44 @@
 import React from 'react'
-import {categories} from '../data/data.js';
+import PuckLight from './PuckLight.jsx';
 
 export const Indoor = () => {
   return (
+    <div>
     <div className='max-w-[1240px] group mx-auto  px-16 bg-[#165a1e] text-white  py-16 sm:rounded-xl'>
-    <div className='flex grid md:grid-cols-3 gap-16'>
+    <div className='grid md:grid-cols-3 gap-16'>
         <div className=' col-span-1'>
         <h1 className='text-4xl mb-8 font-bold uppercase'>Indoor Lighting</h1>
         <p>
         If you're unsure of what a fair price is for a used iPhone, Newton Index can help guide you with our pricing tool. When you're looking to purchase a used iPhone or sell your own, our pricing tool can help you navigate the market and find the best deal.
         </p>
 
+
         </div>
-        <div className=' col-span-2'>
-        <img className='w-fill rounded-xl'src='https://images.unsplash.com/photo-1531973576160-7125cd663d86?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' alt='/'/>
-        </div>
+        <ul className='py-2 text-xl w-[80%] col-span-1'>
+          <li className='border-b py-2'> <a href="#pucklight">Puck Light</a></li>
+          <li className='border-b py-2'>Downlight</li>
+          <li className='border-b py-2'>Decorative Light</li>
+          <li className='border-b py-2'>High/Low Bay</li>
+          <li className='border-b py-2'>Panel Light</li>
+          <li className='border-b py-2'>Luminaires</li>
+        </ul>
+
+        <ul className='py-2 text-xl w-[80%] col-span-1'>
+
+          <li className='border-b py-2'>Grow Light</li>
+          <li className='border-b py-2'>Scones</li>
+          <li className='border-b py-2'>Dock Light</li>
+          <li className='border-b py-2'>T-Bar</li>
+          <li className='border-b py-2'>Track Light</li>
+        </ul>
 
     </div>
+
+
+
+</div>
+
+<PuckLight />
 
 
 </div>
